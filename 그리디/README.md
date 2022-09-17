fc38ff16b058f4f3ce9f55cacb00c7616df1b516
Seojisoo20191941 <amy0480@naver.com>
2022-09-17 23:44:17 +0900

    실전3-1 오답노트

--- 그리디/README.md
+++ 그리디/README.md
@@ -1,3 +1,10 @@
 # 그리디
 
 현재 상황에서 지금 당장 좋은 것만 고르는 방법
+
+input 받을때
+map(int, input().split())
+
+정렬
+sort() 오름차순
+sort(reverse=True) 내림차순
--- 그리디/실전3-1.py
+++ 그리디/실전3-1.py
@@ -5,13 +5,13 @@ array = list(map(int, input().split()))
 # k = 3  # k번 연속 x
 
 # array = [2, 3, 5, 4, 6]
-
-for i in range(n):
-    for j in range(n-i-1):
-        if (array[j] < array[j+1]):
-            temp = array[j]
-            array[j] = array[j+1]
-            array[j+1] = temp
+array.sort(reverse=True)
+# for i in range(n):
+#     for j in range(n-i-1):
+#         if (array[j] < array[j+1]):
+#             temp = array[j]
+#             array[j] = array[j+1]
+#             array[j+1] = temp
 
 result = 0  # 결과값
 num = 0  # 더한 횟수
