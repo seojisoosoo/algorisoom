ed85c21c7ba7dd1ae37ac1309297064619ed3017
Seojisoo20191941 <amy0480@naver.com>
2022-09-23 10:54:15 +0900

    구현 예제4-2

--- 구현/예제4-2.py
+++ 구현/예제4-2.py
@@ -9,10 +9,9 @@ s = 60
 time = [N, m, s]  # 시, 분, 초
 result = 0  # 3이 하나라도 포함되는 경우
 
-# 아직 푸는중!!!
 for i in range(N+1):
-    for j in range(m+1):
-        for k in range(s+1):
+    for j in range(m):
+        for k in range(s):
             if i % 10 == 3 or j//10 == 3 or j % 10 == 3 or k//10 == 3 or k % 10 == 3:
                 result += 1
 
